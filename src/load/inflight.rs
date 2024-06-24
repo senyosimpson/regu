47ed8391cd2fb54a392a78e5f855f8e8a58bbf1c
senyo simpson <simpsonsenyo@gmail.com>
2024-06-24 10:02:12 +0200

    use arc itself as the inflight ref count

--- src/load/inflight.rs
+++ src/load/inflight.rs
@@ -1,51 +1,49 @@
 use std::ops::Deref;
-use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
 use crate::load::Load;
 
 /// Calculate the load based on the number of inflight requests
-// #[derive(Clone)]
+#[derive(Clone)]
 pub struct Inflight<T> {
     inner: T,
-    count: Arc<AtomicU32>,
+    count: RefCount,
 }
+
+/// A reference count. It just uses the number of references to the arc as
+/// the count. This is valid for our case since we clone the object every
+/// time we handle a new request. As it comes in and out of scope, the
+/// count is updated, hence tracking the number of inflight requests.
+///
+/// The refcount is an _estimation_ in that the values might change at the
+/// time of reading the value by other threads. This is fine, we don't need
+/// 100% accuracy for load balancing, over the fullness of time we'll still
+/// have fairly good load balancing
+#[derive(Clone)]
+pub struct RefCount(Arc<()>);
+
+// ===== Inflight =====
 
 impl<T> Inflight<T> {
     pub fn new(inner: T) -> Inflight<T> {
         Inflight {
             inner,
-            count: Arc::new(AtomicU32::new(0)),
+            count: RefCount::new(),
         }
     }
 }
 
 impl<T> Load for Inflight<T> {
-    type Metric = u32;
+    type Metric = usize;
 
     fn load(&self) -> Self::Metric {
-        self.count.load(Ordering::SeqCst)
-    }
-}
-
-impl<T: Clone> Clone for Inflight<T> {
-    fn clone(&self) -> Self {
-        // increase the refcount
-        let count = self.count.clone();
-        // increment the count
-        count.fetch_add(1, Ordering::SeqCst);
-
-        Inflight {
-            inner: self.inner.clone(),
-            count,
-        }
+        Arc::strong_count(&self.count)
     }
 }
 
 impl<T> Drop for Inflight<T> {
     fn drop(&mut self) {
-        let count = self.count.fetch_sub(1, Ordering::SeqCst);
-        println!("prev count: {}", count);
+        println!("prev count: {}", Arc::strong_count(&self.count));
     }
 }
 
@@ -54,5 +52,21 @@ impl<T> Deref for Inflight<T> {
 
     fn deref(&self) -> &Self::Target {
         &self.inner
+    }
+}
+
+// ===== RefCount =====
+
+impl RefCount {
+    fn new() -> RefCount {
+        RefCount(Arc::new(()))
+    }
+}
+
+impl Deref for RefCount {
+    type Target = Arc<()>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
